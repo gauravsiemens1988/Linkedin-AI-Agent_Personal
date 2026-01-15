aed14e4c8ff7f8502a7a99d3541b285c53b25bbc
gauravsiemens1988 <gaurav.siemens1988@gmail.com>
2026-01-15 19:52:22 +0530

    Update fetch_news.py

--- fetch_news.py
+++ fetch_news.py
@@ -1,6 +1,11 @@
 import feedparser
 
-RSS_URL = "https://news.google.com/rss/search?q=green+hydrogen+energy&hl=en-IN&gl=IN&ceid=IN:en"
+RSS_URL = (
+    "https://news.google.com/rss/search?"
+    "q=green+energy+OR+green+hydrogen+OR+solar+OR+renewable+OR+clean+energy"
+    "&hl=en-IN&gl=IN&ceid=IN:en"
+)
+
 
 def fetch_news():
     feed = feedparser.parse(RSS_URL)
